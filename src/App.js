4b1947c31972aa38b371b4dfd3809a5a83b0b7a2
LUIS GUSTAVO SANTOS <LUSANTOS@bbmapfre.com.br>
2018-12-12 13:05:08 -0200

    dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+
+import jwtDecode from 'jwt-decode'
+
 
 class App extends Component {
+  async componentDidMount(){
+    let token = localStorage.getItem('token')
+    if(!token) {
+      const login = await axios.post('http://localhost:3001/users/login', {
+      email: 'tuliofaria@devpleno.com',
+      passwd: 'abc123'
+    })
+    token = login.data.token
+    localStorage.setItem('token', token)
+    }
+    const decoded = jwtDecode(token)
+    console.log(decoded)
+  }
+  
   render() {
     return (
       <div className="App">
